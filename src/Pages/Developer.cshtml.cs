389e4ba9891a9ee4f4229e70e92f70dcacecf6a9
liling yang <yangll2525@163.com>
2022-10-18 14:23:15 -0700

    Added a file named Developer.cshtml.cs; Added injection of Product Service, and call to GetAllData in Developer.cshtml.cs.

---
+++ src/Pages/Developer.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// return all the data to show the developer
+    /// </summary>
+    public class DeveloperModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public DeveloperModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
