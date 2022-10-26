02f05e98bfffe19df0ff87d89abd1740e6091f4b
liling yang <yangll2525@163.com>
2022-10-25 23:35:25 -0700

    Add a Read.cshtml.cs flie; Add OnGet method to get a developer information in Read.cshtml.cs

---
+++ src/Pages/Read.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+using System;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// return all the data to show the developer
+    /// </summary>
+    public class ReadModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ReadModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // single Data
+        public ProductModel Product { get; private set; }
+
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data and find the target data
+        /// </summary>
+        public void OnGet()
+        {
+            var products = ProductService.GetAllData();
+            var id = Request.QueryString.Value;
+            Product = products.FirstOrDefault(x => x.Id.Equals(id.Split("=")[1]));
+        }
+    }
+}
