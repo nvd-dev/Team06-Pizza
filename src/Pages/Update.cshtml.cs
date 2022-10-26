d3c1259c522a4ee4dd49f1241a7f9b97e50d5783
Pui Ching Wong <pwong@seattleu.edu>
2022-10-26 00:06:27 -0700

    Added Update.cshtml.cs to Pages

---
+++ src/Pages/Update.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// return all the data to show the developer
+    /// </summary>
+    public class UpdateModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public UpdateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // single Data
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data and find the target data
+        /// </summary>
+        public void OnGet()
+        {
+            var products = ProductService.GetAllData();
+            Product = products.FirstOrDefault(x => x.Id.Equals("jenlooper-cactus"));
+        }
+    }
+}
