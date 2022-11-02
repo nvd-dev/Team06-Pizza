511cfc7f6a116d43e1cb61b2a91b9e2eaa490252
Pui Ching Wong <pwong@seattleu.edu>
2022-11-01 22:31:09 -0700

    Added unit test for Controllers in ProductsController.Tests.cs

---
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Controllers;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Controllers
+{
+    /// <summary>
+    /// UnitTests for ProductsController
+    /// </summary>
+    public class ProductsControllerTests
+    {
+        #region TestSetup
+
+        public static ProductsController controller;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            controller = new ProductsController(TestHelper.ProductService)
+            {
+            };
+        }
+        #endregion TestSetup
+
+        #region Get
+        [Test]
+        public void Get_Valid_Should_Return_Products()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetAllData().First();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.AreEqual(data.Id, result.First().Id);
+        }
+        #endregion Get
+
+        #region Patch
+        [Test]
+        public void Patch_Valid_Should_Return_Ok()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetAllData().First();
+            var request = new ProductsController.RatingRequest()
+            {
+                ProductId = data.Id,
+                Rating=3
+            };
+
+            // Act
+            var result = controller.Patch(request);
+            var dataNew = TestHelper.ProductService.GetAllData().First();
+
+            // Assert
+            Assert.AreEqual(3, dataNew.Ratings[^1]);
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+        #endregion Patch
+    }
+}
