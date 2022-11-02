2a9f4e1fa9dec56cf8ac128797f2e025e5c2ecfb
NVD <47729520+nvd-dev@users.noreply.github.com>
2022-11-01 21:46:24 -0700

    Added comments to ProductsControllers.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,34 +7,55 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller class to handle incoming requests 
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Contructor for controller class to handle incoming requests
+        /// </summary>
+        /// <param name="productService"> Object contains webhost environment </param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        // ProductService property getter
         public JsonFileProductService ProductService { get; }
 
+
+        // Get method to return all the product services
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetAllData();
         }
 
+        /// <summary>
+        /// Patch method to add rating to a product service
+        /// </summary>
+        /// <param name="request"> Contain rating and productId of the service to update</param>
+        /// <returns> Successful return 200 </returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
             ProductService.AddRating(request.ProductId, request.Rating);
-            
+
             return Ok();
         }
 
+        /// <summary>
+        /// Model for rating a service
+        /// </summary>
         public class RatingRequest
         {
+            // Id of product service to update
             public string ProductId { get; set; }
+
+            // Rating of product service to update
             public int Rating { get; set; }
         }
     }
