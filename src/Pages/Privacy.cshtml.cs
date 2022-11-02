4c3b2de46e7df7a0ccf2870d6802f0340e73e932
NVD <47729520+nvd-dev@users.noreply.github.com>
2022-11-01 21:49:29 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,10 +3,16 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// The privacy policy page 
+    /// </summary>
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Default Constructor; create a logger
+        /// </summary> 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
