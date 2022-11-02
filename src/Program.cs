f738baf2245656f65fd935df2130ca5197297f95
NVD <47729520+nvd-dev@users.noreply.github.com>
2022-11-01 21:49:43 -0700

    Added comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// Entry point of the web application and creates an instance of IWebHost which hosts web application.
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
@@ -10,6 +13,7 @@ namespace ContosoCrafts.WebSite
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Called the startup class in the Startup.cs
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
