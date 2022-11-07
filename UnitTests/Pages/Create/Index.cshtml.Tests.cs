3999bc63576b0ac8d1a0813efc026d85bd566a62
liling yang <yangll2525@163.com>
2022-11-07 00:15:56 -0800

    Add Index.cshtml.Tests.cs in Unitests\Pages\Create folder

---
+++ UnitTests/Pages/Create/Index.cshtml.Tests.cs
@@ -0,0 +1,113 @@
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+
+namespace UnitTests.Pages.Create
+{
+    /// <summary>
+    /// UnitTests for Create
+    /// </summary>
+    public class CreateTests
+    {
+        #region TestSetup
+
+        public static CreateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<CreateModel>>();
+
+            pageModel = new CreateModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Create_Product()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreNotEqual(null, pageModel.Product.Id);
+        }
+        #endregion OnGet
+
+        #region OnPostAsync
+        [Test]
+        public void OnPostAsync_ModelStateIsInvalid_Should_Return_APageResult()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            var pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+            var pageModel = new CreateModel(TestHelper.ProductService)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(actionContext)
+            };
+            pageModel.ModelState.AddModelError("Product.Price", "Value for Price must be between -1 and 100.");
+
+            // Act
+            var result = pageModel.OnPostAsync();
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+        }
+
+        [Test]
+        public void OnPostAsync_ModelStateIsValid_Should_Return_ARedirectToPageResult()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            var pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+            var pageModel = new CreateModel(TestHelper.ProductService)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(actionContext)
+            };
+
+            // Act
+            pageModel.OnGet();
+            var result = pageModel.OnPostAsync();
+
+            // Assert
+            Assert.IsInstanceOf<RedirectToPageResult>(result);
+        }
+        #endregion OnPostAsync
+    }
+}
