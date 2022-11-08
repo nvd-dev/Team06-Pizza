0bc971f8f411fb3524d39957d5e19fed549aed9e
Yu Shang <yshang@seattleu.edu>
2022-11-08 14:00:09 -0800

    Fixed Price Data type bug in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -38,12 +38,12 @@ namespace ContosoCrafts.WebSite.Models
         // A property contain the type of the product
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        // A property contain the quantity of the product
-        public string Quantity { get; set; }
+        // A property contain the quantity of the product, N/A
+        //public string Quantity { get; set; }
 
         // A property contain the product's price
-        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-        public int Price { get; set; }
+        [Range(0, 10000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public uint Price { get; set; }
 
         // Store the Comments entered by the users on this product
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
