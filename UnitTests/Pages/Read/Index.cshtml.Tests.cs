d584825da573d2fc96c3fcda5d2c09bd45b5b684
Yu Shang <yshang@seattleu.edu>
2022-11-20 19:37:25 -0800

    Added comment to Index.cshtml.Tests.cs in Read folder

--- UnitTests/Pages/Read/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Read/Index.cshtml.Tests.cs
@@ -15,9 +15,12 @@ namespace UnitTests.Pages.Read
     public class ReadTests
     {
         #region TestSetup
-
+        // Page Model
         public static ReadModel pageModel;
 
+        /// <summary>
+        /// Setup test
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -32,31 +35,30 @@ namespace UnitTests.Pages.Read
         #endregion TestSetup
 
         #region OnGet
-
-
+        /// <summary>
+        /// test case for OnGet method with valid Id
+        /// </summary>
         [Test]
-        public void OnGet_Valid_Should_Return_Products()
+        public void OnGet_Valid_Id_Should_Return_Products()
         {
-
-            // Arrange
-
-            // Act
+            // Act. Call the OnGet method with valid id
             pageModel.OnGet("jenlooper-cactus");
 
-            // Assert
+            // Assert. Determine whether the page data is correct
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual("jenlooper-cactus", pageModel.Product.Id);
         }
 
+        /// <summary>
+        /// test case for OnGet method with invalid Id
+        /// </summary>
         [Test]
         public void OnGet_InValid_Id_Should_Return_ARedirectToPageResult()
         {
-            // Arrange
-
-            // Act
+            // Act. Call the OnGet method with invalid id
             var result = pageModel.OnGet("test");
 
-            // Assert
+            // Assert. Judging whether the return is correct
             Assert.IsInstanceOf<RedirectToPageResult>(result);
         }
         #endregion OnGet
