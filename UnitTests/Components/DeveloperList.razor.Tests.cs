5f684e0bb1e7fc8e269f25ff80c00193da49a106
liling yang <yangll2525@163.com>
2022-11-23 20:59:22 -0800

    Add unit test for sort function in DeveloperList.razor.Tests.cs

--- UnitTests/Components/DeveloperList.razor.Tests.cs
+++ UnitTests/Components/DeveloperList.razor.Tests.cs
@@ -4,6 +4,9 @@ using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Components;
 using ContosoCrafts.WebSite.Services;
+using Bunit;
+using System.Linq;
+using AngleSharp.Dom;
 
 namespace UnitTests.Components
 {
@@ -44,6 +47,123 @@ namespace UnitTests.Components
             Assert.AreEqual(true, result.Contains("Hiring a Python Developer"));
         }
         #endregion DefaultContent
+
+        #region Sort Developers By Price
+        /// <summary>
+        /// test case for SortDevelopersByPrice function with Aescending order
+        /// </summary>
+        [Test]
+        public void SortDevelopersByPrice_With_Aescending_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act, Rendering Component
+            var page = RenderComponent<DeveloperList>();
+
+            // Click the sorting button
+            page.Find(".sort-by-price").Click();
+
+            // Get price
+            var firstPrice = page.Find(".price").TextContent;
+
+            // Assert, Determine whether the content of the page is correct
+            Assert.AreEqual("0", firstPrice);
+        }
+        /// <summary>
+        /// test case for SortDevelopersByPrice function with Descending order
+        /// </summary>
+        [Test]
+        public void SortDevelopersByPrice_With_Descending_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act, Rendering Component
+            var page = RenderComponent<DeveloperList>();
+
+            // Click the sorting button
+            page.Find(".sort-by-price").Click();
+            page.Find(".sort-by-price").Click();
+
+            // Get price
+            var firstPrice = page.Find(".price").TextContent;
+
+            // Assert, Determine whether the content of the page is correct
+            Assert.AreEqual("30", firstPrice);
+        }
+        #endregion Sort Developers By Price
+
+        #region Sort Developers By Ratings
+        /// <summary>
+        /// test case for SortDevelopersByRatings function with Aescending order
+        /// </summary>
+        [Test]
+        public void SortDevelopersByRatings_With_Aescending_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act, Rendering Component
+            var page = RenderComponent<DeveloperList>();
+
+            // Click the rating button
+            page.Find(".sort-by-ratings").Click();
+
+            // Get ratings
+            var firstRating = page.Find(".ratings-count").TextContent;
+
+            // Assert, Determine whether the content of the page is correct
+            Assert.AreEqual("0", firstRating);
+        }
+
+        /// <summary>
+        /// test case for SortDevelopersByRatings function with Descending order
+        /// </summary>
+        [Test]
+        public void SortDevelopersByRatings_With_Descending_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act, Rendering Component
+            var page = RenderComponent<DeveloperList>();
+
+            // Click the rating button
+            page.Find(".sort-by-ratings").Click();
+            page.Find(".sort-by-ratings").Click();
+
+            // Get ratings
+            var firstRating = page.Find(".ratings-count").TextContent;
+
+            // Assert, Determine whether the content of the page is correct
+            Assert.AreEqual("4", firstRating);
+        }
+        #endregion Sort Developers By Ratings
+
+        #region Reset Sort
+        /// <summary>
+        /// test case for SortDevelopersReset function
+        /// </summary>
+        [Test]
+        public void SortDevelopersReset_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act, Rendering Component
+            var page = RenderComponent<DeveloperList>();
+
+            // Click the reset sorting button
+            page.Find(".sort-reset").Click();
+
+            // Get price
+            var firstPrice = page.Find(".price").TextContent;
+
+            // Assert, Determine whether the content of the page is correct
+            Assert.AreEqual("0", firstPrice);
+        }
+        #endregion Reset Sort
     }
 
 }
